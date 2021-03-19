40ed87f058f4ab8d5d28d5752ebd82c03a42f7dc
Анастасия <anastaciamaz@yandex.ru>
2021-03-19 13:47:22 +0300

    "@qualifier" annotations added

--- src/main/java/ru/spring/MusicPlayer.java
+++ src/main/java/ru/spring/MusicPlayer.java
@@ -1,20 +1,23 @@
 package ru.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
+
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String PlayMusic() {
-        return "playing classic " + rockMusic.getSong();
+        return "playing classic " + music2.getSong();
     }
 }
