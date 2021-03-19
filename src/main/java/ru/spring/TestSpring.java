cc468e50de37610439bb5512beda65ac9dc342f3
Анастасия <anastaciamaz@yandex.ru>
2021-03-19 12:00:45 +0300

    init and destroy behavior with prototype bean

--- src/main/java/ru/spring/TestSpring.java
+++ src/main/java/ru/spring/TestSpring.java
@@ -9,8 +9,13 @@ public class TestSpring
                 "applicationContext.xml"
         );
 
-        ClassicalMusic music = context.getBean("musicBean", ClassicalMusic.class);
-        System.out.println(music.getSong());
+        ClassicalMusic music1 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic music2 = context.getBean("musicBean", ClassicalMusic.class);
+        ClassicalMusic music3 = context.getBean("musicBean", ClassicalMusic.class);
+
+        System.out.println(music1.getSong()+"1");
+        System.out.println(music2.getSong()+"2");
+        System.out.println(music3.getSong()+"2");
 
 //        Music music = context.getBean("musicBean",Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
